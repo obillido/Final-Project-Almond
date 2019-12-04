fcce9c3de8d855ff6d0bb530cd65d7351d02d2d3
Suhyeon <shkim416@gmail.com>
2019-12-04 11:08:34 +0900

    부트스트랩....ㅠ

---
+++ src/main/java/project/app/almond/controller/WebtoonController.java
@@ -0,0 +1,12 @@
+package project.app.almond.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebtoonController {
+	@RequestMapping("/webtoon/webtoonpage")
+	public String main(){
+		return "webtoon/webtoonpage";
+	}
+}
