9ddfe43c4d3dbfa8291bd032f7f7614931f48522
yun <cyh89221@naver.com>
2019-12-12 14:23:58 +0900

    티켓 history

--- src/main/java/project/app/almond/controller/TicketHistoryContorller.java
+++ src/main/java/project/app/almond/controller/TicketHistoryContorller.java
@@ -1,22 +1,21 @@
 package project.app.almond.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import project.app.almond.service.TicketBuyService;
-import project.app.almond.vo.TicketBuyVo;
+import project.app.almond.service.TicketUseService;
 
 @Controller
 public class TicketHistoryContorller {
 	@Autowired private TicketBuyService tbs;
+	@Autowired private TicketUseService tus;
 	@RequestMapping("/ticket/history")
-	public String ticketInvoice(int usernum, Model model){
-		List<TicketBuyVo> his=tbs.history(usernum);
-		model.addAttribute("list",his);
+	public String tickethistory(int usernum, Model model){
+		model.addAttribute("tbh",tbs.history(usernum));
+		model.addAttribute("tuh",tus.history(usernum));
 		return ".ticket.history";
 	}
 }
