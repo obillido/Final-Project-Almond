f51a6d5f202c7fce5af93441d75c15a62dd7e37e
obillido <obillido@naver.com>
2019-12-18 09:08:17 +0900

    스케쥴러 테스트중

---
+++ src/main/java/project/app/almond/scheduler/DynamicAbstractScheduler.java
@@ -0,0 +1,29 @@
+package project.app.almond.scheduler;
+
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+public abstract class DynamicAbstractScheduler {
+	private ThreadPoolTaskScheduler scheduler;
+	
+	public void stopScheduler(){
+		scheduler.shutdown();
+	}
+	public void startScheduler(){
+		scheduler = new ThreadPoolTaskScheduler();
+		scheduler.initialize();
+		scheduler.schedule(getRunnable(), getTrigger());
+	}
+	
+	private Runnable getRunnable(){
+		return new Runnable(){
+			@Override
+			public void run() {
+				runner();
+			}
+		};
+	}
+	
+	public abstract void runner();
+	public abstract Trigger getTrigger();
+}
